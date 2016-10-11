d5a0249e539f5c1db8b69e25627388ff2ea3a937
Hoang Vu <nguyenhoangvu1803@gmail.com>
2016-10-11 07:11:14 +0700

    up

--- wp-content/themes/phuongtoan/inc/header/header.php
+++ wp-content/themes/phuongtoan/inc/header/header.php
@@ -9,61 +9,7 @@ global $smof_data, $zo_meta;
 
 // Get Width Header
 $container = (!empty($smof_data['header_width']) && $smof_data['header_width']) ? 'container-fluid' : 'container';
-
-// Socials Style
-$socials_style = (!empty($smof_data['tb_socials_style'])) ? $smof_data['tb_socials_style'] : '';
-$socials_style = (!empty($zo_meta->_zo_tbar_socials_style)) ? $zo_meta->_zo_tbar_socials_style : $socials_style;
-$top_bar_show = !empty($zo_meta->_zo_top_bar_show) ? $zo_meta->_zo_top_bar_show: '';
 ?>
-
-<!-- Header Top -->
-<?php if($top_bar_show != 'none') {?>
-	<div id="zo-header-top">
-		<div class="<?php echo esc_attr($container);?>">
-			<div class="row">
-				<?php 
-					if(!empty($smof_data['tbar_section'])){
-						$left = 0;
-						foreach($smof_data['tbar_section'] as $key=>$value){
-							switch($key){
-								case '1':
-									if($value){ ?>
-										<div class="col-lg-6 col-md-6 col-sm-6 hidden-xs <?php if($left) echo 'align-right'; else echo 'align-left';?>">
-											<div class="top-bar-infomation">
-												<div class="infomation-content"><?php if(!empty($smof_data['tbar_infomation'])) print($smof_data['tbar_infomation']);?></div>
-											</div>
-										</div>
-									<?php }
-									break;
-								case '2':
-									if($value){ ?>
-										<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 <?php if($left) echo 'align-right'; else echo 'align-left';?>">
-											<?php 
-												if(!empty($smof_data['tb_social'])){
-													echo '<div class="social-ul '. esc_attr($socials_style) .'"><ul>';
-														foreach($smof_data['tb_social'] as $key=>$value){
-															switch($value){
-																case 1:
-																	$href_key = 'social_link_' . esc_attr($key);
-																	$href = $smof_data[$href_key];
-																	echo '<li class="'. esc_attr($key) .'"><a href="'. esc_attr($href) .'" target="_blank"><i class="fa fa-'. esc_attr($key) .'"></i></a></li>';
-																	break;
-															}
-														}
-													echo '</ul></div>';
-												}
-											?>
-										</div>
-									<?php }
-									break;
-							} $left++;
-						}
-					}
-				?>
-			</div>
-		</div>
-	</div>
-<?php }?>
 
 <!-- Header Navigation -->
 <div id="zo-header" class="zo-main-header header-default <?php if ($smof_data['menu_sticky_tablets']) {
